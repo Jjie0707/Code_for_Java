475c5d2e3ebd5919e33caa9d59da198acac0663d
Jjie0707 <1778836683@qq.com>
2025-03-08 00:01:38 +0800

    第一次

---
+++ Jjie_3_7/src/Test/Person.java
@@ -0,0 +1,22 @@
+package Test;
+
+public class Person {
+    String name;
+    int age;
+    String sex;
+
+    public Person(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
---
+++ Jjie_3_7/src/Test/test.java
@@ -0,0 +1,9 @@
+package Test;
+
+
+public class test {
+    public static void main(String[] args) {
+        Person p1=new Person("Jjie",18,"男");
+        System.out.println(p1);
+    }
+}
