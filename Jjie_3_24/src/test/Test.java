701b404ad9f5912bdd121da78f76ffc4d2571548
Jjie0707 <1778836683@qq.com>
2025-03-24 22:59:18 +0800

    Update Test.java

--- Jjie_3_24/src/test/Test.java
+++ Jjie_3_24/src/test/Test.java
@@ -2,6 +2,23 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
+        String s1="abc";
+        String s2="abc";
+        System.out.println(s1);
+        System.out.println(s2);
+
+        String s3=new String("abc");
+        System.out.println(s3);
+
+        char[] ch={'a','b','c'};
+        String s4=new String(ch);
+        System.out.println(s4);
+
+        byte[] ch1={97,98,99};
+        String s5=new String(ch1);
+        System.out.println(s5);
+    }
+    public static void main0(String[] args) {
         System.out.println("Before");
         int[] arr= {1, 2, 3};
         try{
